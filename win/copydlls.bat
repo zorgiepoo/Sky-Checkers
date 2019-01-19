3bf25397e19c23fbd3681737ebde2a193ce5ed3d
Zorg <zorgiepoo@gmail.com>
2019-01-19 12:00:40 -0800

    Copy zlib1.dll and create bat file for copying dlls

---
+++ win/copydlls.bat
@@ -0,0 +1,22 @@
+xcopy /y /d "%~1\glew-2.1.0\bin\Release\Win32\glew32.dll" "%~2"
+
+xcopy /y /d "%~1\glew-2.1.0\LICENSE.glew.txt" "%~2"
+
+xcopy /y /d "%~1\SDL2-2.0.9\lib\x86\SDL2.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\SDL2_mixer.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\libogg-0.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\libvorbis-0.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\libvorbisfile-3.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\LICENSE.ogg-vorbis.txt" "%~2"
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\SDL2_ttf.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\libfreetype-6.dll" "%~2"
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\zlib1.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\LICENSE.freetype.txt" "%~2"
