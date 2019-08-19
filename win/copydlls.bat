94bfe5eb23876a6718c969ecb643d7358997d116
Zorg <zorgiepoo@gmail.com>
2019-08-18 21:56:47 -0700

    Migrate windows project to produce x64 binaries

--- win/copydlls.bat
+++ win/copydlls.bat
@@ -1,22 +1,33 @@
-xcopy /y /d "%~1\glew-2.1.0\bin\Release\Win32\glew32.dll" "%~2"
+xcopy /y /d "%~1\glew-2.1.0\bin\Release\x64\glew32.dll" "%~2"
+
 
 xcopy /y /d "%~1\glew-2.1.0\LICENSE.glew.txt" "%~2"
 
-xcopy /y /d "%~1\SDL2-2.0.9\lib\x86\SDL2.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\SDL2_mixer.dll" "%~2"
+xcopy /y /d "%~1\SDL2-2.0.9\lib\x64\SDL2.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\libogg-0.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\libvorbis-0.dll" "%~2"
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x64\SDL2_mixer.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\libvorbisfile-3.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x86\LICENSE.ogg-vorbis.txt" "%~2"
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x64\libogg-0.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\SDL2_ttf.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\libfreetype-6.dll" "%~2"
-xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\zlib1.dll" "%~2"
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x64\libvorbis-0.dll" "%~2"
 
-xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x86\LICENSE.freetype.txt" "%~2"
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x64\libvorbisfile-3.dll" "%~2"
+
+
+xcopy /y /d "%~1\SDL2_mixer-2.0.4\lib\x64\LICENSE.ogg-vorbis.txt" "%~2"
+
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x64\SDL2_ttf.dll" "%~2"
+
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x64\libfreetype-6.dll" "%~2"
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x64\zlib1.dll" "%~2"
+
+
+xcopy /y /d "%~1\SDL2_ttf-2.0.14\lib\x64\LICENSE.freetype.txt" "%~2"
