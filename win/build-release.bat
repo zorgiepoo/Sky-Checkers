6d941c3ca7795c3dd67ced087056fe8a753f364c
Zorg <zorgiepoo@gmail.com>
2020-04-04 13:15:16 -0700

    Add build script on Windows for distribution

---
+++ win/build-release.bat
@@ -0,0 +1,7 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild" /t:Rebuild /p:Configuration=Release
+if exist x64\SkyCheckers rmdir /s/q x64\SkyCheckers
+mkdir x64\SkyCheckers
+robocopy ..\Data x64\SkyCheckers\Data\ /e /NFL /NDL /NJH /NJS
+robocopy x64\Release x64\SkyCheckers\ SkyCheckers.exe *.dll /NFL /NDL /NJH /NJS
+mkdir x64\SkyCheckers\Licenses
+robocopy x64\Release x64\SkyCheckers\Licenses LICENSE*.txt /NFL /NDL /NJH /NJS
