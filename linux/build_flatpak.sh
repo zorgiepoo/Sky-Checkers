50b67800d65b3231a44c15e882d3efa5210641af
Zorg <zorgiepoo@gmail.com>
2020-04-26 10:20:21 -0700

    Rename flatpak build output directory

--- linux/build_flatpak.sh
+++ linux/build_flatpak.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-flatpak-builder build-dir com.zgcoder.skycheckers.json --force-clean
+flatpak-builder flatpak-build com.zgcoder.skycheckers.json --force-clean
 #flatpak-builder --run build-dir com.zgcoder.skycheckers.json skycheckers
 
