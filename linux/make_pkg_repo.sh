70ce5b4bfc5f1a36c2f9609484ec44c2fab2658b
Zorg <zorgiepoo@gmail.com>
2020-05-03 20:50:31 -0700

    Include icons in the repo instead of the extra data

--- linux/make_pkg_repo.sh
+++ linux/make_pkg_repo.sh
@@ -6,7 +6,6 @@ rm -f sc-snap-pak-repo.tgz
 rm -f sc-linux-data.tgz
 cp -R ../../Sky-Checkers /tmp/sc-snap-pak-repo
 rm -f /tmp/sc-snap-pak-repo/linux/skycheckers*.txt
-rm -rf /tmp/sc-snap-pak-repo/linux/icons
 rm -rf /tmp/sc-snap-pak-repo/linux/flatpak-build
 rm -rf /tmp/sc-snap-pak-repo/linux/.flatpak-builder
 rm -rf /tmp/sc-snap-pak-repo/linux/Data
@@ -17,7 +16,6 @@ rm -rf /tmp/sc-snap-pak-repo/linux/Shaders
 rm -f /tmp/sc-snap-pak-repo/linux/*.sh
 rm -f /tmp/sc-snap-pak-repo/linux/snapcraft.yaml
 rm -f /tmp/sc-snap-pak-repo/linux/com.zgcoder.skycheckers.json
-rm -f /tmp/sc-snap-pak-repo/linux/com.zgcoder.skycheckers.appdata.xml
 rm -rf /tmp/sc-snap-pak-repo/mac
 rm -rf /tmp/sc-snap-pak-repo/win
 rm -rf /tmp/sc-snap-pak-repo/Data
