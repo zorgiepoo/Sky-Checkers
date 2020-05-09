6dbaa4d5ca64eafc2d99400eb4a7aa9bbeef6348
Zorg <zorgiepoo@gmail.com>
2020-05-08 18:21:05 -0700

    Add git submodule for flathub shared-modules

--- linux/build_flatpak.sh
+++ linux/build_flatpak.sh
@@ -2,6 +2,9 @@
 #flatpak-builder flatpak-build net.zgcoder.skycheckers.json --force-clean
 #flatpak-builder --run build-dir net.zgcoder.skycheckers.json skycheckers
 
+#https://github.com/flathub/shared-modules
+#git submodule add https://github.com/flathub/shared-modules.git
+
 flatpak remove --user -y net.zgcoder.skycheckers
 flatpak-builder --gpg-sign=$GPGKEY --repo=sc-repo --force-clean flatpak-build net.zgcoder.skycheckers.json
 flatpak --user remote-add --if-not-exists sc-repo sc-repo
