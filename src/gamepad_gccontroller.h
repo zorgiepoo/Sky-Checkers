218a12dfeba19797c466b847af7164f1de442247
Zorg <zorgiepoo@gmail.com>
2020-09-26 11:11:37 -0700

    Disable keyboard support for tvOS

--- src/gamepad_gccontroller.h
+++ src/gamepad_gccontroller.h
@@ -24,7 +24,8 @@
 #if PLATFORM_IOS
 #define GC_NAME(x) x
 #define GC_PRODUCT_CHECK 0
-#define GC_KEYBOARD 1
+// tvOS doesn't properly support keyboard events (as of tvOS 14.0)
+#define GC_KEYBOARD !PLATFORM_TVOS
 #else
 #define GC_NAME(x) GC_##x
 #define GC_PRODUCT_CHECK 1
