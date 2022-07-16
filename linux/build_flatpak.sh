08f599bec61c93026f510620e8a8eaf3cff73a00
Zorg <zorgiepoo@gmail.com>
2022-07-15 21:08:43 -0700

    Add alternative build/test instructions for flat_pak

--- linux/build_flatpak.sh
+++ linux/build_flatpak.sh
@@ -10,3 +10,8 @@ flatpak-builder --gpg-sign=$GPGKEY --repo=sc-repo --force-clean flatpak-build ne
 flatpak --user remote-add --if-not-exists sc-repo sc-repo
 flatpak --user install sc-repo net.zgcoder.skycheckers
 
+# Alternatively to test:
+#flatpak remove --user -y net.zgcoder.skycheckers
+#flatpak-builder --repo=sc-repo --force-clean flatpak-build net.zgcoder.skycheckers.json
+#flatpak-builder --user --install --force-clean build-dir net.zgcoder.skycheckers.json
+#flatpak run net.zgcoder.skycheckers
