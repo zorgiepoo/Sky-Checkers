856d1868de9431b09e5731a99a9520ce50f99f38
Zorg <zorgiepoo@gmail.com>
2026-06-28 18:00:59 -0700

    Clear cracked render on predicted tiles

--- src/scenery.c
+++ src/scenery.c
@@ -131,6 +131,8 @@ void _clearPredictedColor(int tileIndex)
 	if (gTiles[tileIndex].coloredID == NO_CHARACTER)
 	{
 		restoreDefaultTileColor(tileIndex);
+		gTiles[tileIndex].crackedTime = 0.0f;
+		gTiles[tileIndex].cracked = false;
 	}
 }
 
